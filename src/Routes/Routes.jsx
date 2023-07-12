c44cd89b7ba33eff21625f8ea178e55bbbf1969b
francois <francoisgrunert@gmail.com>
2023-07-12 11:21:49 +0200

    style(home): add tab to select alcool or not

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 //~ Import Module
 import { Route } from "react-router-dom";
-import { NoPage, Home, Login, Drink, Dashboard } from "../Pages";
+import { NoPage, Home, Login, Drink, Dashboard, Search } from "../Pages";
 
 const Router = [
   { id: 1, mainPath: "*", mainElement: <NoPage /> },
@@ -8,6 +8,7 @@ const Router = [
   { id: 3, mainPath: "/login", mainElement: <Login /> },
   { id: 4, mainPath: "/drink", mainElement: <Drink /> },
   { id: 5, mainPath: "/dashboard", mainElement: <Dashboard /> },
+  { id: 6, mainPath: "/search", mainElement: <Search /> },
 ];
 
 const mainRoutes = Router.map(({ id, mainPath, mainElement }) => <Route key={id} path={mainPath} element={mainElement} />);
