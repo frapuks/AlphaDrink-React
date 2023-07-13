7e4d08712259738b08f91d654554a44c95383af3
francois <francoisgrunert@gmail.com>
2023-07-13 10:43:29 +0200

    style(footer): add links

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,11 +1,18 @@
-import { Box, Typography } from "@mui/material";
+import { IconButton, Stack, Typography } from "@mui/material";
 import "./Footer.scss"
+import { GitHub, LinkedIn } from "@mui/icons-material";
 
 const Footer = () => {
   return (
-    <Box component="footer" className="footer" position="sticky" top="100%" sx={{backgroundColor:"primary.dark"}}>
-      <Typography variant="overline">Footer</Typography>
-    </Box>
+    <Stack component="footer" className="footer" position="sticky" top="100%" direction='row' sx={{backgroundColor:"primary.dark"}}>
+      <IconButton href="https://www.linkedin.com/in/francois-grunert/" target="blank">
+        <LinkedIn/>
+      </IconButton>
+      <Typography variant="overline">Frapuks - 2023</Typography>
+      <IconButton href="https://github.com/frapuks">
+        <GitHub/>
+      </IconButton>
+    </Stack>
   );
 };
 
